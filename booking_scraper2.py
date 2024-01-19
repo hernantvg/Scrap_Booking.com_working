65818c70f056b3b92c29ef1e15ab6fb55fcf9550
hernantvg <111532382+hernantvg@users.noreply.github.com>
2024-01-19 16:26:40 -0300

    Update booking_scraper2.py

--- booking_scraper2.py
+++ booking_scraper2.py
@@ -80,7 +80,7 @@ def main():
         hotels_list = []  # Initialize outside the loop
 
         # Adjust the range to get more pages if needed
-        for page_number in range(1, 8):
+        for page_number in range(1, 4):
             page_url = f'{base_url}&offset={25 * (page_number - 1)}'
             page.goto(page_url, timeout=120000)
 
