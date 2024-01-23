0b867aba986426b3cc4ff1c36c28baf73fc4f6af
hernantvg <hernantvg@gmail.com>
2024-01-23 09:27:22 -0300

    s

--- booking_scraper.py
+++ booking_scraper.py
@@ -136,7 +136,7 @@ def main():
     base_url = f'https://www.booking.com/searchresults.{language}.html?&checkin={checkin_date}&checkout={checkout_date}&selected_currency=USD&ss={city}&ssne={city}&ssne_untouched={city}&lang={language}&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
 
     # Adjust the range to get more pages if needed
-    hotels_list = scrape_all_pages(base_url, page_count=8, city=city, country=country)
+    hotels_list = scrape_all_pages(base_url, page_count=34, city=city, country=country)
 
     # Add description to each hotel
     for hotel in hotels_list:
