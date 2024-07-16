1b06f5714e573b8fbb55166453cbe9ec1a4cb090
hernantvg <111532382+hernantvg@users.noreply.github.com>
2024-07-16 18:10:47 -0300

    Update booking_scraper-v2.py

--- booking_scraper-v2.py
+++ booking_scraper-v2.py
@@ -83,16 +83,15 @@ def scrape_popular_facilities_with_retry(hotel_url, max_retries=3):
             response = requests.get(hotel_url)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
-                facilities_elements = soup.find_all(
-                    'span', class_='a5a5a75131')
-                facilities_list = [facility.get_text(
-                    strip=True) for facility in facilities_elements]
-                return facilities_list
+                facilities_elements = soup.select('div.e2585683de.fcfa97735d')
+                facilities_list = [facility.get_text(strip=True) for facility in facilities_elements]
+                facilities_str = ", ".join(facilities_list)
+                return facilities_str
             else:
-                return ["Error al acceder a la página del hotel."]
+                return "Error al acceder a la página del hotel."
         except Exception as e:
             logging.error(f"Error en el intento {retry + 1}: {str(e)}")
-    return ["Error: Se ha excedido el número máximo de intentos."]
+    return "Error: Se ha excedido el número máximo de intentos."
 
 # Función para extraer los datos de los hoteles en una página
 
